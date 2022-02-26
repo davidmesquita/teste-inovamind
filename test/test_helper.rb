82c07070ca9f469b6b28627523d3922c8d0acfc8
davidmesquita <devid.mesquita13@gmail.com>
2022-02-26 14:35:53 -0300

    add mongo config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,8 +6,11 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
+<<<<<<< HEAD
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+=======
+>>>>>>> add mongo config
   # Add more helper methods to be used by all tests here...
 end
